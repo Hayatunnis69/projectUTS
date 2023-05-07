22d10968112ff35a1d7780c48e98504ab9e3e975
Hayatunnis69 <atunhayatun49@gmail.com>
2023-05-07 22:07:00 +0700

    project uts

---
+++ admin/layouts/menu.php
@@ -0,0 +1,33 @@
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Welcome
+                            </a>
+                            <a class="nav-link collapsed" href="list_produk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Produk
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <a class="nav-link collapsed" href="list_jenis_produk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Jenis Produk
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <a class="nav-link" href="list_pesanan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pesanan
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
